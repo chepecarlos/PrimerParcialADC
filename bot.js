57e57779263604ea300c00ffe2fa3891f3eaacc6
herbertdubon <hdubon@tecnasa.com>
2018-02-26 11:29:37 -0600

    probando codigo

---
+++ bot.js
@@ -0,0 +1,32 @@
+
+process.env["NTBA_FIX_319"] = 1;
+process.env["NTBA_FIX_319"] = 1;
+const Bot = require('node-telegram-bot-api');
+const request = require('request');
+
+const url = 'https://launchlibrary.net/1.3/launch';
+const trigger = 'Me quieres';
+const token = '547644896:AAHvXTQRmzXgGiEZjAM_m44YqrwmbumCqpA';
+const bot = new Bot(token, {polling: true});
+
+const prepareData = (body) => {
+ const launches = JSON.parse(body).launches;
+ return launches.filter((launch) => launch !== undefined)
+  .map((launch) => `${launch.name} on ${launch.net}`)
+  .join('\n\n');
+};
+
+bot.on('message', (msg) => {
+ if (msg.text.toString() === trigger) {
+  return request(url, (err, resp, body) => {
+   bot.sendMessage(msg.chat.id, prepareData(body));
+  });
+ }
+
+bot.sendMessage(msg.chat.id, 'Hi, do you want to travel?', {
+  reply_markup: {
+    keyboard: [[trigger], ['Bulk option']]
+   }
+  }
+ );
+});
